b8382c607d04f781eb1656ae8c0a573b7f119e5e
Mario T. Lanza <mlanza@doesideas.com>
2024-07-27 18:58:17 -0400

    create part map

--- libs/ecs/serial-map.js
+++ libs/ecs/serial-map.js
@@ -1,30 +1,29 @@
 import _ from "../atomic_/core.js";
-import $ from "../atomic_/shell.js";
 
-export function Stash(index, serialize, deserialize){
+export function SerialMap(index, serialize, deserialize){
   this.serialize = serialize;
   this.deserialize = deserialize;
   this.index = index;
 }
 
-export function stash(entries, index = {}, serialize = JSON.stringify, deserialize = JSON.parse){
+export function serialMap(entries, index = {}, serialize = JSON.stringify, deserialize = JSON.parse){
   return _.reduce(function(memo, [key, value]){
     return assoc(memo, key, value);
-  }, new Stash(index, serialize, deserialize), entries);
+  }, new SerialMap(index, serialize, deserialize), entries);
 }
 
-export const map = stash;
+export const map = serialMap;
 
 function lookup(self, key){
   return _.get(self.index, self.serialize(key));
 }
 
 function assoc(self, key, value){
-  return new Stash(_.assoc(self.index, self.serialize(key), value), self.serialize, self.deserialize);
+  return new SerialMap(_.assoc(self.index, self.serialize(key), value), self.serialize, self.deserialize);
 }
 
 function dissoc(self, key){
-  return new Stash(_.dissoc(self.index, self.serialize(key)), self.serialize, self.deserialize);
+  return new SerialMap(_.dissoc(self.index, self.serialize(key)), self.serialize, self.deserialize);
 }
 
 function contains(self, key){
@@ -41,7 +40,7 @@ function seq(self){
   }, _.seq(self.index)) : null;
 }
 
-$.doto(Stash,
+_.doto(SerialMap,
   _.implement(_.ILookup, {lookup}),
   _.implement(_.IAssociative, {assoc, contains}),
   _.implement(_.ISeqable, {seq}),
--- libs/ecs_/serial-set.js
+++ libs/ecs_/serial-set.js
@@ -1,3 +1,3 @@
 import _ from "../atomic_/core.js";
-import * as mod from "../ecs/stash.js";
+import * as mod from "../ecs/serial-set.js";
 export default _.impart(mod, _.partly);
