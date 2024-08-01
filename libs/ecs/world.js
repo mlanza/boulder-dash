e19d8b271cf68676abe4bb9431f72984e64d6298
Mario T. Lanza <me@mlanza.com>
2024-08-01 17:07:13 -0400

    reorg

--- libs/ecs/world.js
+++ libs/ecs/world.js
@@ -33,85 +33,6 @@ export function world(inputs, tags){
     }, _, tags));
 }
 
-function changed2(reel, ...path){
-  const id = _.first(path);
-  const compared = r.correlate(reel, _.getIn(_, path));
-  const [curr, prior] = compared;
-  const touched = r.correlate(reel, _.getIn(_, path), r.touched);
-  const keys = path.length === 1 ? _.union(_.keys(curr), _.keys(prior)) : null;
-  const components = touched ? _.reduce(function(memo, key){
-    const touched = r.correlate(reel, _.getIn(_, [id, key]), r.touched);
-    touched && $.assoc(memo, key, touched);
-    return memo;
-   }, {}, keys) : {};
-  return {id, touched, components, compared};
-}
-
-function changed1(reel){
-  return _.chain(reel, r.frame, function(world){
-    return world.db.touched;
-  }, _.mapa(_.partial(changed2, reel), _));
-}
-
-export const changed = _.overload(null, changed1, changed2);
-
-function touched(init = s.set([])){
-  return install(["touched"], init, r.modified, function(id){
-    return _.conj(_, id);
-  });
-}
-
-export function via(tag, init = sm.map([])){
-  const props = _.assoc({}, tag, _.isSome);
-  const pattern = {props};
-  return install(["via", tag], init, _.plug(r.modified, _, {props: [tag], pattern}), function(id, {reel, triggered}){
-    const touched = _.getIn(triggered || {}, ["props", tag]);
-    if (!touched) {
-      return _.identity;
-    } else {
-      const [curr, prior] = r.correlate(reel, _.getIn(_, [id, tag]));
-      return _.pipe(
-        _.includes(["removed", "updated"], touched) ? _.dissoc(_, prior) : _.identity,
-        _.includes(["added", "updated"], touched) ? _.assoc(_, curr, id) : _.identity);
-    }
-  });
-}
-
-function component(tag, init =  s.set([])){
-  const props = _.assoc({}, tag, _.includes(["added", "removed"], _));
-  const pattern = {props};
-  return install(["components", tag], init, _.plug(r.modified, _, {props: [tag], pattern}), function(id, {triggered}){
-    const {props} = triggered || {};
-    const touched = _.get(props, tag);
-    const f = touched === "added" ? _.conj : _.disj;
-    return f(_, id);
-  });
-}
-
-function install(path, init, trigger, update){
-  return function(self){
-    return new World(
-      self.entities,
-      self.inputs,
-      _.assocIn(self.db, path, init),
-      _.conj(self.hooks, {path, trigger, update}));
-  }
-}
-
-function hooks(id, prior){
-  return function(self){
-    const reel = r.edit(self, prior);
-    return new World(
-      self.entities,
-      self.inputs,
-      _.reduce(function(db, {path, trigger, update}){
-        const triggered = trigger(id)(reel);
-        return triggered ? _.updateIn(db, path, update(id, {reel, triggered})) : db;
-      }, self.db, self.hooks),
-      self.hooks);
-  }
-}
-
 function lookup(self, id){
   return _.get(self.entities, id);
 }
@@ -153,6 +74,64 @@ $.doto(World,
   _.implement(_.ISeqable, {seq}),
   _.implement(c.ICapture, {capture})); //TODO implement frame
 
+//concrete fns
+function install(path, init, trigger, update){
+  return function(self){
+    return new World(
+      self.entities,
+      self.inputs,
+      _.assocIn(self.db, path, init),
+      _.conj(self.hooks, {path, trigger, update}));
+  }
+}
+
+function hooks(id, prior){
+  return function(self){
+    const reel = r.edit(self, prior);
+    return new World(
+      self.entities,
+      self.inputs,
+      _.reduce(function(db, {path, trigger, update}){
+        const triggered = trigger(id)(reel);
+        return triggered ? _.updateIn(db, path, update(id, {reel, triggered})) : db;
+      }, self.db, self.hooks),
+      self.hooks);
+  }
+}
+
+function touched(init = s.set([])){
+  return install(["touched"], init, r.modified, function(id){
+    return _.conj(_, id);
+  });
+}
+
+function component(tag, init =  s.set([])){
+  const props = _.assoc({}, tag, _.includes(["added", "removed"], _));
+  const pattern = {props};
+  return install(["components", tag], init, _.plug(r.modified, _, {props: [tag], pattern}), function(id, {triggered}){
+    const {props} = triggered || {};
+    const touched = _.get(props, tag);
+    const f = touched === "added" ? _.conj : _.disj;
+    return f(_, id);
+  });
+}
+
+export function via(tag, init = sm.map([])){
+  const props = _.assoc({}, tag, _.isSome);
+  const pattern = {props};
+  return install(["via", tag], init, _.plug(r.modified, _, {props: [tag], pattern}), function(id, {reel, triggered}){
+    const touched = _.getIn(triggered || {}, ["props", tag]);
+    if (!touched) {
+      return _.identity;
+    } else {
+      const [curr, prior] = r.correlate(reel, _.getIn(_, [id, tag]));
+      return _.pipe(
+        _.includes(["removed", "updated"], touched) ? _.dissoc(_, prior) : _.identity,
+        _.includes(["added", "updated"], touched) ? _.assoc(_, curr, id) : _.identity);
+    }
+  });
+}
+
 export function having(self, components){
   return _.chain(components,
     _.mapa(_.get(self.db.components, _), _),
@@ -172,3 +151,25 @@ export function bestow(...args){
   const fs = _.initial(args), xs = _.last(args);
   return _.map(_.juxt(_.identity, ...fs), xs);
 }
+
+function changed2(reel, ...path){
+  const id = _.first(path);
+  const compared = r.correlate(reel, _.getIn(_, path));
+  const [curr, prior] = compared;
+  const touched = r.correlate(reel, _.getIn(_, path), r.touched);
+  const keys = path.length === 1 ? _.union(_.keys(curr), _.keys(prior)) : null;
+  const components = touched ? _.reduce(function(memo, key){
+    const touched = r.correlate(reel, _.getIn(_, [id, key]), r.touched);
+    touched && $.assoc(memo, key, touched);
+    return memo;
+   }, {}, keys) : {};
+  return {id, touched, components, compared};
+}
+
+function changed1(reel){
+  return _.chain(reel, r.frame, function(world){
+    return world.db.touched;
+  }, _.mapa(_.partial(changed2, reel), _));
+}
+
+export const changed = _.overload(null, changed1, changed2);
