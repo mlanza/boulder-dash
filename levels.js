778c30770d90db4744d4cfa4906bd12e3d72db90
Mario T. Lanza <mlanza@doesideas.com>
2024-08-10 11:04:43 -0400

    fix rolling prioritizing non-pinned items, add level

--- levels.js
+++ levels.js
@@ -1,6 +1,7 @@
 export default [{
   cave: "A",
   title: "Intro",
+  summary: "Pick up jewels and exit before time is up",
   author: "Peter Liepa",
   time: 150,
   size: [40, 22],
@@ -32,6 +33,7 @@ export default [{
 }, {
   cave: "B",
   title: "Rooms",
+  summary: "Pick up jewels, but you must move boulders to get all jewels",
   author: "Peter Liepa",
   time: 150,
   size: [40, 22],
@@ -58,5 +60,36 @@ export default [{
     W.....r.wr..d...w... r..w..r....w...rr W
     W.d... rw..r....w.Xd r..w. .....w...rr W
     W.r.... w.. ..r.w.P. ...w....r.rw.... .W
+    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW`
+}, {
+  cave: "C",
+  title: "Maze",
+  summary: "Pick up jewels. You must get every jewel to exit",
+  author: "Peter Liepa",
+  time: 150,
+  size: [40, 22],
+  diamonds: {needed: 10, worth: 15, extras: 0},
+  map: `
+    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+    Wr.ww.wrr.w...rwr..r....w...r.....rw.d.W
+    W..Xw.d.r.w...www..w.r....r..r.r...w.wrW
+    W....w..rd..r....w.....r.wwr.......w.wwW
+    Wd.w..wrwr..r....w...r......r.rr......wW
+    Wr.w...w..r.ww..r.wwd.......r.rr......wW
+    Wrr..r....w...r......r.rr......r..dww..W
+    W..r.ww..r.rr...w....r.rr......w..r.w.rW
+    W..w...d......d.r..wwr..r.w.wr..wr..d.rW
+    Wr.r....w.ww..d.r..wwr..r..d.w...w..r.wW
+    W.r.ww.....rrwr..d.w.wr..wr...wr..d.r..W
+    Ww.ww......rrwr..r.w.ww...w..r.ww..r.wwW
+    W.w.r.r.w...wwr..r....w...r.....ww.r.wwW
+    W.w.r.r.w.d.w.wr..wr....r..r.rr....w...W
+    Ww..wrwr..r....w...d...w.rw......w.ww.dW
+    Ww...wwr..w.d...wr..r.r...r.wr......w..W
+    Ww.d....r.ww..r.wwr.......r.wr......w..W
+    W..r....w...r......r.rr......w..r.w...wW
+    Wr.ww..r.ww...w....r.rr......w..rd..r..W
+    Ww...r......r.rd......r...ww..wr..d.w..W
+    Wrr...w.....r.rd......w..r.wd.d.rw.r...W
     WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW`
 }]
