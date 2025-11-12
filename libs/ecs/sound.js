62f72b1e1d9288a4b858cc72017bcf35666ae01e
Mario T. Lanza <me@mlanza.com>
2025-11-12 17:26:00 -0500

    slight sound debounce

--- libs/ecs/sound.js
+++ libs/ecs/sound.js
@@ -4,6 +4,7 @@ import {IAudible} from "./iaudible.js";
 
 export function Sound(file){
   this.file = file;
+  this.timer = null;
 }
 
 export function sound(file){
@@ -13,7 +14,11 @@ export function sound(file){
 function play(sound, loop = false){
   const audio = sound.audio = new Audio(sound.file);
   audio.loop = loop;
-  audio.play();
+  sound.timer || audio.play();
+  clearTimeout(sound.timer);
+  sound.timer = setTimeout(function(){
+    sound.timer = null;
+  }, 50);
 }
 
 function pause(sound){
